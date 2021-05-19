83ba7a5ccb9e6598c6d1dd50d98ef5f792c3eef7
DuleD <dusaborota@protonmail.com>
2021-05-19 15:44:33 +0200

    domaci dodao negativne TC

--- cypress/integration/examples/login.spec.js
+++ cypress/integration/examples/login.spec.js
@@ -10,24 +10,77 @@ describe('nevalidan login', () => {
         cy.get('a[href="/login"]').click()
     })*/
 
-    it('click login button', () => {
+    it('Click login button', () => {
         cy.get('a[class="nav-link nav-buttons"]').eq(0).click() //eq je ugradjena funkcija koja hvata (element)
     })
 
-    it('enter user name', () => {
-        cy.get('input[id="email"]').type('highhookgmail.com') //cy.wait(6000) iznad cy.get ceka pre nego sto odradi test
-    })
+    it('Login form', () => {
 
-    it('enter password', () => {
+        /*//cy.get('input[id="email"]').type('highhook@gmail.com')
+        //cy.get('input[id="password"]').type('12345678')
+        cy.get('button[class="btn btn-custom"]').click()    //cy.get('button[class="btn btn-custom"]').type('{enter}') pritisne enter umesto sto klikne na dugme
+        cy.wait(2000)
+        cy.reload()
+
+        //cy.get('input[id="email"]').type('highhook@gmail.com')
         cy.get('input[id="password"]').type('12345678')
+        cy.get('button[class="btn btn-custom"]').click()
+        cy.wait(2000)
+        cy.reload()
+
+        cy.get('input[id="email"]').type('highhook@gmail.com')
+        //cy.get('input[id="password"]').type('12345678')
+        cy.get('button[class="btn btn-custom"]').click()
+        cy.wait(2000)
+        cy.reload()
+
+        cy.get('input[id="email"]').type('highhasdasdsdook@gmail.com')  //pogresan mail
+        cy.get('input[id="password"]').type('12345678')
+        cy.get('button[class="btn btn-custom"]').click()
+        cy.wait(2000)
+        cy.reload()
+
+        cy.get('input[id="email"]').type('highhook@gmail.com')
+        cy.get('input[id="password"]').type('123456780000000000')  //pogresan pass
+        cy.get('button[class="btn btn-custom"]').click()
+        cy.wait(2000)
+        cy.reload()
+
+        cy.get('input[id="email"]').type('highhookgmail.com')  //nevalidan email format
+        cy.get('input[id="password"]').type('12345678')
+        cy.get('button[class="btn btn-custom"]').click()
+        cy.wait(2000)
+        cy.reload()
+
+        cy.get('input[id="email"]').type('highhook@gmailђшђшђшђ.com')  //specijalni karakteri u mailu
+        cy.get('input[id="password"]').type('12345678')
+        cy.get('button[class="btn btn-custom"]').click()
+        cy.wait(2000)
+        cy.reload()
+
+        cy.get('input[id="email"]').type('direktorat@gmail.com')
+        cy.get('input[id="password"]').type('д1234567')    //specijalni karakteri u passu
+        cy.get('button[class="btn btn-custom"]').click()
+        cy.wait(2000)
+        cy.reload()
+
+        cy.get('input[id="email"]').type('direktorat@gmail.com')
+        cy.get('input[id="password"]').type('D1234567')     //password case sensitive D umesto d
+        cy.get('button[class="btn btn-custom"]').click()
+        cy.wait(2000)
+        cy.reload()*/
+
+        cy.get('input[id="email"]').type('DIREKTORAT@gmail.com')  //email case sensitive
+        cy.get('input[id="password"]').type('d1234567')
+        cy.get('button[class="btn btn-custom"]').click()
+        cy.wait(2000)
+        cy.reload()
+
     })
 
-    it('click submit', () => {
-        cy.get('button[class="btn btn-custom"]').click()
-    })
 })
 
-describe('login spec', () => {
+/*describe('login spec', () => {
     it('visit gallery app', () => {
         cy.visit('https://gallery-app.vivifyideas.com/') // ako posle it stavimo it.only onda ce se samo taj test izvrsiti
         //ako u cypress.json upisemo baseurl mozemo da ostavimo prazno cy.visit a ako dodamo /blabla otici ce na baseurl/blabla
@@ -37,7 +90,7 @@ describe('login spec', () => {
         cy.get('a[href="/login"]').click()
     })*/
 
-    it('click login button', () => {
+    /*it('click login button', () => {
         cy.get('a[class="nav-link nav-buttons"]').eq(0).click() //eq je ugradjena funkcija koja hvata (element)
     })
 
@@ -61,8 +114,8 @@ describe('login spec', () => {
     })
 })*/
 
-describe('logout', () => {
+/*describe('logout', () => {
     it('click logout', () =>{
         cy.get('.ml-auto > :nth-child(3) > .nav-link').click() //preko cypress snipera
     })
-})
+})*/
--- cypress/integration/examples/register.spec.js
+++ cypress/integration/examples/register.spec.js
@@ -1,5 +1,3 @@
-
-
 describe('negativan register', () => {
     it('open gallery app', () => {
         cy.visit('')
@@ -9,15 +7,121 @@ describe('negativan register', () => {
         cy.get('a[href="/register"]').click()
     })
 
+    function makeid(length) {
+        var result           = '';
+        var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        var charactersLength = characters.length;
+        for ( var i = 0; i < length; i++ ) {
+           result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+     }
+
     it('fill register form', () => {
-        //cy.get('input[id="first-name"]').type('') prazan username ili ga samo izbrisemo da ne popunjava jer je default prazan
+        /*//cy.get('input[id="first-name"]').type('') prazan username ili ga samo izbrisemo da ne popunjava jer je default prazan
         cy.get('input[id="last-name"]').type('Regal')
         cy.get('input[id="email"]').type('vecnaslava@rusija.com')
         cy.get('input[id="password"]').type('12345678')
         cy.get('input[id="password-confirmation"]').type('12345678')
         cy.get('input[class="form-check-input"]').check()
         cy.get('button[class="btn btn-custom"]').click()
+        cy.wait(2000)
+        cy.reload()
+
+        cy.get('input[id="first-name"]').type('Steva')
+        //cy.get('input[id="last-name"]').type('Regal')
+        cy.get('input[id="email"]').type('vecnaslava@rusija.com')
+        cy.get('input[id="password"]').type('12345678')
+        cy.get('input[id="password-confirmation"]').type('12345678')
+        cy.get('input[class="form-check-input"]').check()
+        cy.get('button[class="btn btn-custom"]').click()
+        cy.wait(2000)
+        cy.reload()
+
+        cy.get('input[id="first-name"]').type('Steva')
+        cy.get('input[id="last-name"]').type('Regal')
+        //cy.get('input[id="email"]').type('vecnaslava@rusija.com')
+        cy.get('input[id="password"]').type('12345678')
+        cy.get('input[id="password-confirmation"]').type('12345678')
+        cy.get('input[class="form-check-input"]').check()
+        cy.get('button[class="btn btn-custom"]').click()
+        cy.wait(2000)
+        cy.reload()
+
+        cy.get('input[id="first-name"]').type('Steva')
+        cy.get('input[id="last-name"]').type('Regal')
+        cy.get('input[id="email"]').type('vecnaslava@rusija.com')
+        //cy.get('input[id="password"]').type('12345678')
+        cy.get('input[id="password-confirmation"]').type('12345678')
+        cy.get('input[class="form-check-input"]').check()
+        cy.get('button[class="btn btn-custom"]').click()
+        cy.wait(2000)
+        cy.reload()
+
+        cy.get('input[id="first-name"]').type('Steva')
+        cy.get('input[id="last-name"]').type('Regal')
+        cy.get('input[id="email"]').type('vecnaslava@rusija.com')
+        cy.get('input[id="password"]').type('12345678')
+        //cy.get('input[id="password-confirmation"]').type('12345678')
+        cy.get('input[class="form-check-input"]').check()
+        cy.get('button[class="btn btn-custom"]').click()
+        cy.wait(2000)
+        cy.reload()
+
+        cy.get('input[id="first-name"]').type('Steva')
+        cy.get('input[id="last-name"]').type('Regal')
+        cy.get('input[id="email"]').type('vecnaslava@rusija.com')
+        cy.get('input[id="password"]').type('12345678')
+        cy.get('input[id="password-confirmation"]').type('12345678')
+        //cy.get('input[class="form-check-input"]').check()
+        cy.get('button[class="btn btn-custom"]').click()
+        cy.wait(2000)
+        cy.reload()
+
+        cy.get('input[id="first-name"]').type('Steva')
+        cy.get('input[id="last-name"]').type('Regal')
+        cy.get('input[id="email"]').type('vecnaslavarusija.com') //bez @
+        cy.get('input[id="password"]').type('12345678')
+        cy.get('input[id="password-confirmation"]').type('12345678')
+        cy.get('input[class="form-check-input"]').check()
+        cy.get('button[class="btn btn-custom"]').click()
+        cy.wait(4000)
+        cy.reload()
+
+        cy.get('input[id="first-name"]').type('Steva')
+        cy.get('input[id="last-name"]').type('Regal')
+        cy.get('input[id="email"]').type(makeid(6) + '@rusija.com')
+        cy.get('input[id="password"]').type('1234')        //short password
+        cy.get('input[id="password-confirmation"]').type('1234')
+        cy.get('input[class="form-check-input"]').check()
+        cy.get('button[class="btn btn-custom"]').click()
+        cy.wait(2000)
+        cy.reload()
+
+        cy.get('input[id="first-name"]').type('Steva')
+        cy.get('input[id="last-name"]').type('Regal')
+        cy.get('input[id="email"]').type(makeid(6) + '@rusija.com')
+        cy.get('input[id="password"]').type('12345678')
+        cy.get('input[id="password-confirmation"]').type('11111111') //wrong confirm
+        cy.get('input[class="form-check-input"]').check()
+        cy.get('button[class="btn btn-custom"]').click()
+        cy.wait(2000)
+        cy.reload()*/
+
+        cy.get('input[id="first-name"]').type('Steva')
+        cy.get('input[id="last-name"]').type('Regal')
+        cy.get('input[id="email"]').type(makeid(6) + '@чћжћ') //special characters in email
+        cy.get('input[id="password"]').type('12345678')
+        cy.get('input[id="password-confirmation"]').type('12345678')
+        cy.get('input[class="form-check-input"]').check()
+        cy.get('button[class="btn btn-custom"]').click()
+        cy.wait(2000)
+        cy.reload()
     })
+
+    
+
+
 })
 
 /*describe('register spec', () => {
